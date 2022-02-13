0a5c097858925acbe56d1b5c86caf8dabc61d56a
iberatkaya <beratkaya1998@gmail.com>
2022-02-13 00:46:54 -0600

    Updated user card to show user info

--- lib/presentation/pages/home/widgets/user_card.dart
+++ lib/presentation/pages/home/widgets/user_card.dart
@@ -1,8 +1,10 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:helping_hands_sponty/application/auth/auth_cubit.dart';
 
 class UserCard extends StatelessWidget {
   const UserCard({
@@ -47,126 +49,130 @@ class UserCard extends StatelessWidget {
             ),
             Positioned(
               top: -4,
-              child: Container(
-                width: 248.0,
-                height: 81.0,
-                decoration: BoxDecoration(
-                  // color: Colors.red.withOpacity(0.3),
-                  borderRadius: BorderRadius.circular(16),
-                  // border: Border.all(
-                  //   width: 1.0,
-                  //   color: const Color(0x40000000),
-                  // ),
-                  boxShadow: const [
-                    BoxShadow(
-                      blurStyle: BlurStyle.outer,
-                      // color: Color(0x40000000),
-                      offset: Offset(0, 4),
-                      blurRadius: 4.0,
+              child: BlocBuilder<AuthCubit, AuthState>(
+                builder: (context, state) {
+                  return Container(
+                    width: 248.0,
+                    height: 81.0,
+                    decoration: BoxDecoration(
+                      // color: Colors.red.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(16),
+                      // border: Border.all(
+                      //   width: 1.0,
+                      //   color: const Color(0x40000000),
+                      // ),
+                      boxShadow: const [
+                        BoxShadow(
+                          blurStyle: BlurStyle.outer,
+                          // color: Color(0x40000000),
+                          offset: Offset(0, 4),
+                          blurRadius: 4.0,
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(width: 18),
-                    Column(
+                    child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
+                        const SizedBox(width: 18),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                              "assets/svg/profile.svg",
+                              // color: Colors.white,
+                              // width: 24,
+                              // height: 24,
+                            ),
+                            Text(
+                              state.userModel.bloodTypeString,
+                              textAlign: TextAlign.center,
+                              maxLines: 1,
+                              softWrap: false,
+                              overflow: TextOverflow.ellipsis,
+                              style: GoogleFonts.quicksand(
+                                color: const Color(0xFFFFFFFF),
+                                fontSize: 12,
+                                // letterSpacing: -0.41,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(width: 18),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                state.userModel.name,
+                                textAlign: TextAlign.start,
+                                maxLines: 1,
+                                softWrap: false,
+                                overflow: TextOverflow.ellipsis,
+                                style: GoogleFonts.quicksand(
+                                  color: const Color(0xFFFFFFFF),
+                                  fontSize: 16,
+                                  // letterSpacing: -0.41,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                              const SizedBox(height: 2),
+                              Text(
+                                state.userModel.phoneNumber,
+                                textAlign: TextAlign.start,
+                                maxLines: 1,
+                                softWrap: false,
+                                overflow: TextOverflow.ellipsis,
+                                style: GoogleFonts.quicksand(
+                                  color: const Color(0xFFFFFFFF),
+                                  fontSize: 12,
+                                  // letterSpacing: -0.41,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              const SizedBox(height: 5),
+                              Text(
+                                "${state.userModel.emergencyContactName}:",
+                                textAlign: TextAlign.start,
+                                maxLines: 1,
+                                softWrap: false,
+                                overflow: TextOverflow.ellipsis,
+                                style: GoogleFonts.quicksand(
+                                  color: const Color(0xFFFFFFFF),
+                                  fontSize: 8,
+                                  // letterSpacing: -0.41,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              Text(
+                                state.userModel.emergencyContactNumber,
+                                textAlign: TextAlign.start,
+                                maxLines: 1,
+                                softWrap: false,
+                                overflow: TextOverflow.ellipsis,
+                                style: GoogleFonts.quicksand(
+                                  color: const Color(0xFFFFFFFF),
+                                  fontSize: 12,
+                                  // letterSpacing: -0.41,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(width: 12),
                         SvgPicture.asset(
-                          "assets/svg/profile.svg",
+                          "assets/svg/edit.svg",
                           // color: Colors.white,
                           // width: 24,
                           // height: 24,
                         ),
-                        Text(
-                          "A rh+",
-                          textAlign: TextAlign.center,
-                          maxLines: 1,
-                          softWrap: false,
-                          overflow: TextOverflow.ellipsis,
-                          style: GoogleFonts.quicksand(
-                            color: const Color(0xFFFFFFFF),
-                            fontSize: 12,
-                            // letterSpacing: -0.41,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
+                        const SizedBox(width: 10),
                       ],
                     ),
-                    const SizedBox(width: 18),
-                    Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Aytunc Isseven",
-                            textAlign: TextAlign.start,
-                            maxLines: 1,
-                            softWrap: false,
-                            overflow: TextOverflow.ellipsis,
-                            style: GoogleFonts.quicksand(
-                              color: const Color(0xFFFFFFFF),
-                              fontSize: 16,
-                              // letterSpacing: -0.41,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          const SizedBox(height: 2),
-                          Text(
-                            "+90 533 261 3880",
-                            textAlign: TextAlign.start,
-                            maxLines: 1,
-                            softWrap: false,
-                            overflow: TextOverflow.ellipsis,
-                            style: GoogleFonts.quicksand(
-                              color: const Color(0xFFFFFFFF),
-                              fontSize: 12,
-                              // letterSpacing: -0.41,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          const SizedBox(height: 5),
-                          Text(
-                            "Annesi:",
-                            textAlign: TextAlign.start,
-                            maxLines: 1,
-                            softWrap: false,
-                            overflow: TextOverflow.ellipsis,
-                            style: GoogleFonts.quicksand(
-                              color: const Color(0xFFFFFFFF),
-                              fontSize: 8,
-                              // letterSpacing: -0.41,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                          Text(
-                            "+90 535 365 3409",
-                            textAlign: TextAlign.start,
-                            maxLines: 1,
-                            softWrap: false,
-                            overflow: TextOverflow.ellipsis,
-                            style: GoogleFonts.quicksand(
-                              color: const Color(0xFFFFFFFF),
-                              fontSize: 12,
-                              // letterSpacing: -0.41,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(width: 12),
-                    SvgPicture.asset(
-                      "assets/svg/edit.svg",
-                      // color: Colors.white,
-                      // width: 24,
-                      // height: 24,
-                    ),
-                    const SizedBox(width: 10),
-                  ],
-                ),
+                  );
+                },
               ),
             ),
           ],
